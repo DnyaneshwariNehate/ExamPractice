806f06cdd64c6dffc729b8c55746f198636c3fc9
shraddha <shraddha@gmail.com>
2023-01-25 01:17:21 +0530

    First git practice commit

---
+++ ExamPractice/src/main/java/practice/exam/sdm/SDMExamPractice.java
@@ -0,0 +1,12 @@
+package practice.exam.sdm;
+
+import java.io.Console;
+
+public class SDMExamPractice {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World...");
+
+	}
+
+}
